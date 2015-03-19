de56b0709ee2c60547cd3489ac114e20c76221a3
ChanderG <chandergovind@gmail.com>
2015-03-19 21:22:04 +0530

    one more func for 1

--- list_basic/1.rkt
+++ list_basic/1.rkt
@@ -13,7 +13,17 @@
     [(empty? (rest l)) (car l)]
     [else (lst2 (rest l))]))  
 
+#| a sample foldl: note the init is used at the end
+(define (concat l)
+  (foldl string-append "Start" l))
+(concat (list "Hi" "There"))
+|#
 
+;textbook foldl case
+;learning - (x) means a function, don't simply add brackets like in other languages
+(define (lst3 l)
+  (foldl (lambda (x y) x) 0 l))
 
 (lst1 (list "Hi" "There"))
 (lst2 (list "Hi" "There"))
+(lst3 (list "Hi" "There"))
