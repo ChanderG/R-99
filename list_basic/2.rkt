690d7145b37e165eb162cd2779db2e2bc55dc2cc
ChanderG <chandergovind@gmail.com>
2015-03-27 18:48:47 +0530

    adds some more sol for 2

--- list_basic/2.rkt
+++ list_basic/2.rkt
@@ -9,5 +9,17 @@
 (list 1 2 3 4)
 (lst1 (list 1 2 3 4)) 
 
+; one more way to manage the lists
+(lst1 '(1 2 3 4))
 
+; yet another way
+; not - no brackets around lst => it is not a procedure
+(define lst
+  '(1 2 3 4))
+(lst1 lst)
 
+; another way of solving
+(second (take (reverse lst) 2))
+
+; obv, does not take care of common cases
+(second (take (reverse '(1)) 2))
